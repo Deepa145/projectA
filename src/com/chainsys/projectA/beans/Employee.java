8e4d5739493c6389f8420796c7903560e3db4c1e
Deepa145 <Deepa.ravi@chainsys.com>
2022-06-08 17:58:48 +0530

    Spring Beans

---
+++ src/com/chainsys/projectA/beans/Employee.java
@@ -0,0 +1,26 @@
+package com.chainsys.projectA.beans;
+
+public class Employee
+{
+	private int id;
+public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+private String name;
+
+public void print()
+{
+System.out.println(this.getId());	
+System.out.println(this.getName());
+}
+}
--- src/com/chainsys/projectA/startup/Main.java
+++ src/com/chainsys/projectA/startup/Main.java
@@ -1,9 +1,34 @@
 package com.chainsys.projectA.startup;
+import com.chainsys.projectA.beans.Customer;
+import com.chainsys.projectA.beans.Employee;
+import org.springframework.context.ApplicationContext; // parent
+import org.springframework.context.support.ClassPathXmlApplicationContext; // child
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		testB();
+         //testA();
 	}
 
+	public static void testA() {
+		Employee emp = new Employee();
+		emp.setId(100);
+		emp.setName("Gopika");
+		emp.print();
+	}
+
+	public static void testB() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		//Employee emp = ac.getBean(Employee.class);
+		Employee emp=(Employee) ac.getBean("emp");
+		emp.setId(101);
+		emp.setName("Geetha");
+		emp.print();
+		
+		Customer c=ac.getBean(Customer.class);
+		c.setId(111);
+		c.setName("Deepa");
+		c.print();
+	}
 }
